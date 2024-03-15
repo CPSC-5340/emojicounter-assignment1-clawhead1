b131852b419a8d4dc0b1ece81f2d72fac5fe8836
clawhead1 <156616990+clawhead1@users.noreply.github.com>
2024-03-15 18:23:59 -0500

    Add files via upload

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let icons = ["🤣", "😜", "😀", "🤩", "😂"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                List{
+                    ForEach(icons, id: \.self) {
+                        emoji in myCounter(icon: emoji, count: 0)
+                    }
+                    
+                }
+            }
+            .navigationBarTitle("Emoji Counter", displayMode:.inline)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct myCounter: View {
+    var icon: String
+    @State var count : Int
+    
+    var body: some View {
+        HStack {
+            Text(icon)
+            Text("Counter: \(count)")
+            Spacer()
+            ZStack {
+                Image(systemName: "plus")
+                    .onTapGesture {
+                        count += 1
+                    }
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.red, lineWidth: 2)
+            }
+            .frame(width: 30, height: 30)
+            ZStack {
+                Image(systemName: "minus")
+                    .onTapGesture {
+                        count -= 1
+                    }
+                RoundedRectangle(cornerRadius: 10).stroke(Color.red, lineWidth: 2)
+            }
+            .frame(width: 42, height: 30)
+            
+        }
+        .padding()
+        .foregroundColor(.red)
     }
 }
